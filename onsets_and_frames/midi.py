b5212635ad62948c9379ac9328bc31ebf75498e0
Cliff Njoroge <37869706+cliffordkleinsr@users.noreply.github.com>
2024-03-01 11:28:00 +0300

    Added Files

---
+++ onsets_and_frames/midi.py
@@ -0,0 +1,111 @@
+import multiprocessing
+import sys
+
+import mido
+import pretty_midi
+import numpy as np
+import collections
+from joblib import Parallel, delayed
+from mir_eval.util import hz_to_midi
+from tqdm import tqdm
+
+
+def parse_midi(path):
+    """open midi file and return np.array of (onset, offset, note, velocity) rows"""
+    midi = mido.MidiFile(path)
+
+    time = 0
+    sustain = False
+    events = []
+    for message in midi:
+        time += message.time
+
+        if message.type == 'control_change' and message.control == 64 and (message.value >= 64) != sustain:
+            # sustain pedal state has just changed
+            sustain = message.value >= 64
+            event_type = 'sustain_on' if sustain else 'sustain_off'
+            event = dict(index=len(events), time=time, type=event_type, note=None, velocity=0)
+            events.append(event)
+
+        if 'note' in message.type:
+            # MIDI offsets can be either 'note_off' events or 'note_on' with zero velocity
+            velocity = message.velocity if message.type == 'note_on' else 0
+            event = dict(index=len(events), time=time, type='note', note=message.note, velocity=velocity, sustain=sustain)
+            events.append(event)
+
+    notes = []
+    for i, onset in enumerate(events):
+        if onset['velocity'] == 0:
+            continue
+
+        # find the next note_off message
+        offset = next(n for n in events[i + 1:] if n['note'] == onset['note'] or n is events[-1])
+
+        if offset['sustain'] and offset is not events[-1]:
+            # if the sustain pedal is active at offset, find when the sustain ends
+            offset = next(n for n in events[offset['index'] + 1:] if n['type'] == 'sustain_off' or n is events[-1])
+
+        note = (onset['time'], offset['time'], onset['note'], onset['velocity'])
+        notes.append(note)
+
+    return np.array(notes)
+
+
+def save_midi(path, pitches, intervals, velocities):
+    """
+    Save extracted notes as a MIDI file
+    Parameters
+    ----------
+    path: the path to save the MIDI file
+    pitches: np.ndarray of bin_indices
+    intervals: list of (onset_index, offset_index)
+    velocities: list of velocity values
+    """
+    file = pretty_midi.PrettyMIDI()
+    piano_program = pretty_midi.instrument_name_to_program('Acoustic Grand Piano')
+    piano = pretty_midi.Instrument(program=piano_program)
+
+    # Remove overlapping intervals (end time should be smaller of equal start time of next note on the same pitch)
+    intervals_dict = collections.defaultdict(list)
+    for i in range(len(pitches)):
+        pitch = int(round(hz_to_midi(pitches[i])))
+        intervals_dict[pitch].append((intervals[i], i))
+    for pitch in intervals_dict:
+        interval_list = intervals_dict[pitch]
+        interval_list.sort(key=lambda x: x[0][0])
+        for i in range(len(interval_list) - 1):
+            # assert interval_list[i][1] <= interval_list[i+1][0], f'End time should be smaller of equal start time of next note on the same pitch. It was {interval_list[i][1]}, {interval_list[i+1][0]} for pitch {key}'
+            interval_list[i][0][1] = min(interval_list[i][0][1], interval_list[i+1][0][0])
+
+    for pitch in intervals_dict:
+        interval_list = intervals_dict[pitch]
+        for interval,i in interval_list:
+            pitch = int(round(hz_to_midi(pitches[i])))
+            velocity = int(127*min(velocities[i], 1))
+            note = pretty_midi.Note(velocity=velocity, pitch=pitch, start=interval[0], end=interval[1])
+            piano.notes.append(note)
+
+    file.instruments.append(piano)
+    file.write(path)
+
+
+if __name__ == '__main__':
+
+    def process(input_file, output_file):
+        midi_data = parse_midi(input_file)
+        np.savetxt(output_file, midi_data, '%.6f', '\t', header='onset\toffset\tnote\tvelocity')
+
+
+    def files():
+        for input_file in tqdm(sys.argv[1:]):
+            if input_file.endswith('.mid'):
+                output_file = input_file[:-4] + '.tsv'
+            elif input_file.endswith('.midi'):
+                output_file = input_file[:-5] + '.tsv'
+            else:
+                print('ignoring non-MIDI file %s' % input_file, file=sys.stderr)
+                continue
+
+            yield (input_file, output_file)
+
+    Parallel(n_jobs=multiprocessing.cpu_count())(delayed(process)(in_file, out_file) for in_file, out_file in files())
---
+++ transcribe.py
@@ -0,0 +1,115 @@
+import numpy as np, librosa, sys, argparse, torch, os
+
+from mir_eval.util import midi_to_hz
+from torch import dtype
+from pathlib import Path
+from tqdm.auto import tqdm
+
+#from onsets_and_frames import *
+from onsets_and_frames.utils import save_pianoroll
+from onsets_and_frames.constants import SAMPLE_RATE, HOP_LENGTH, MIN_MIDI, DEFAULT_DEVICE
+from onsets_and_frames.mel import melspectrogram
+from onsets_and_frames.decoding import extract_notes
+from onsets_and_frames.midi import save_midi
+
+
+def float_samples_to_int16(y):
+  """Convert floating-point numpy array of audio samples to int16."""
+  # From https://github.com/tensorflow/magenta/blob/671501934ff6783a7912cc3e0e628fd0ea2dc609/magenta/music/audio_io.py#L48
+  if not issubclass(y.dtype.type, np.floating):
+    raise ValueError('input samples not floating-point')
+  return (y * np.iinfo(np.int16).max).astype(np.int16)
+
+
+def load_and_process_audio(flac_path, sequence_length, device):
+
+    random = np.random.RandomState(seed=42)
+
+    audio, sr = librosa.load(flac_path, sr=SAMPLE_RATE)
+    audio = float_samples_to_int16(audio)
+    
+    assert sr == SAMPLE_RATE
+    assert audio.dtype == 'int16'
+
+    audio = torch.ShortTensor(audio)
+
+    if sequence_length is not None:
+        audio_length = len(audio)
+        step_begin = random.randint(audio_length - sequence_length) // HOP_LENGTH
+        n_steps = sequence_length // HOP_LENGTH
+
+        begin = step_begin * HOP_LENGTH
+        end = begin + sequence_length
+
+        audio = audio[begin:end].to(device)
+    else:
+        audio = audio.to(device)
+
+    audio = audio.float().div_(32768.0)
+
+    return audio
+
+
+def transcribe(model, audio):
+
+    mel = melspectrogram(audio.reshape(-1, audio.shape[-1])[:, :-1]).transpose(-1, -2)
+    onset_pred, offset_pred, _, frame_pred, velocity_pred = model(mel)
+
+    predictions = {
+            'onset': onset_pred.reshape((onset_pred.shape[1], onset_pred.shape[2])),
+            'offset': offset_pred.reshape((offset_pred.shape[1], offset_pred.shape[2])),
+            'frame': frame_pred.reshape((frame_pred.shape[1], frame_pred.shape[2])),
+            'velocity': velocity_pred.reshape((velocity_pred.shape[1], velocity_pred.shape[2]))
+        }
+
+    return predictions
+
+
+def transcribe_file(model_file, audio_paths, save_path, sequence_length,
+                  onset_threshold, frame_threshold, device):
+
+    model = torch.load(model_file, map_location=device).eval()
+    #summary(model)
+    for i,audio_path in enumerate(tqdm(audio_paths)):
+        #print(f'{i+1}/{len(list(audio_paths))}: Processing {audio_path}...', file=sys.stderr)
+        audio = load_and_process_audio(audio_path.as_posix(), sequence_length, device)
+        predictions = transcribe(model, audio)
+
+        p_est, i_est, v_est = extract_notes(predictions['onset'], predictions['frame'], predictions['velocity'], onset_threshold, frame_threshold)
+
+        scaling = HOP_LENGTH / SAMPLE_RATE
+
+        i_est = (i_est * scaling).reshape(-1, 2)
+        p_est = np.array([midi_to_hz(MIN_MIDI + midi) for midi in p_est])
+
+        os.makedirs(save_path, exist_ok=True)
+        #pred_path = os.path.join(save_path, os.path.basename(audio_path) + '.pred.png')
+        #save_pianoroll(pred_path, predictions['onset'], predictions['frame'])
+        midi_path = os.path.join(save_path, os.path.basename(audio_path) + '.mid')
+        save_midi(midi_path, p_est, i_est, v_est)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--model', type=str, default='model-500000.pt')
+    parser.add_argument('-i', '--input', type=Path, default=None, help='Input folder')
+    parser.add_argument(
+            "-r",
+            "--recursive",
+            action="store_true",
+            help="whether to search directory recursively",
+    )
+    parser.add_argument('--save-path', type=str, default='.')
+    parser.add_argument('--sequence-length', default=None, type=int)
+    parser.add_argument('--onset-threshold', default=0.5, type=float)
+    parser.add_argument('--frame-threshold', default=0.5, type=float)
+    parser.add_argument('--device', default=DEFAULT_DEVICE)
+    args = parser.parse_args()
+
+    if args.recursive:
+        filenames = args.input.rglob("*wav")
+    else:
+        filenames = args.input.glob("*wav")
+    with torch.no_grad():
+        #transcribe_file(**vars(parser.parse_args()))
+      transcribe_file(args.model, filenames, args.save_path, args.sequence_length, args.onset_threshold, args.frame_threshold, args.device)
